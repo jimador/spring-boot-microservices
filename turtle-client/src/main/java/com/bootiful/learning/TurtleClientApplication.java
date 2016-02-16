6b82456b940de5bda8b0ac0113a7f2344ce77b9b
jamesd1184 <jamesd1184@gmail.com>
2016-02-16 12:14:07 -0500

    initial turtle client

---
+++ turtle-client/src/main/java/com/bootiful/learning/TurtleClientApplication.java
@@ -0,0 +1,12 @@
+package com.bootiful.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleClientApplication.class, args);
+	}
+}
