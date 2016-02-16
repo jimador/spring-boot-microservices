520e7878d196d1c9e2e3cc9e14018342bea115cb
jamesd1184 <jamesd1184@gmail.com>
2016-02-16 12:23:10 -0500

    enable turtle client

--- turtle-client/src/main/java/com/bootiful/learning/TurtleClientApplication.java
+++ turtle-client/src/main/java/com/bootiful/learning/TurtleClientApplication.java
@@ -2,8 +2,12 @@ package com.bootiful.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class TurtleClientApplication {
 
 	public static void main(String[] args) {
