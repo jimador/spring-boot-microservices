acff384ae126f575b663233ff87e356f41f0d57f
jamesd1184 <jamesd1184@gmail.com>
2016-02-16 10:50:56 -0500

    adding config-service

---
+++ config-service/src/main/java/com/bootiful/learning/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bootiful.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
