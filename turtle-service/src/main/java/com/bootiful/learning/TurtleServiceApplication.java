2be90e0f4ae979df18fc38289ba0ee17a985c028
jamesd1184 <jamesd1184@gmail.com>
2016-02-16 08:37:11 -0500

    turtle service to get those turtles

---
+++ turtle-service/src/main/java/com/bootiful/learning/TurtleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bootiful.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleServiceApplication.class, args);
+	}
+}
