05ec5a5ddb4924d1b9d280d95bc52b46b4d2bc9c
jamesd1184 <jamesd1184@gmail.com>
2016-02-16 09:35:36 -0500

    controllers and services

--- turtle-service/src/main/java/com/bootiful/learning/model/Turtle.java
+++ turtle-service/src/main/java/com/bootiful/learning/model/Turtle.java
@@ -1,7 +1,22 @@
 package com.bootiful.learning.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by james on 2/16/2016.
  */
+@Entity
+@Data
 public class Turtle {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String type;
+    private Integer healthPoints;
 }
--- turtle-service/src/main/java/com/bootiful/learning/repositories/TurtleRepository.java
+++ turtle-service/src/main/java/com/bootiful/learning/repositories/TurtleRepository.java
@@ -1,7 +1,16 @@
 package com.bootiful.learning.repositories;
 
+import com.bootiful.learning.model.Turtle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
 /**
  * Created by james on 2/16/2016.
  */
-public class TurtleRepository {
+@Repository
+public interface TurtleRepository extends JpaRepository<Turtle, Long>{
+    Collection<Turtle> findByTurtleName(@Param("name") String name);
 }
