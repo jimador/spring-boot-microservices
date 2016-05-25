32e78583bd6981e63f0ea2a81d3d0723a38e3466
james.dunnam <jamesd1184@gmail.com>
2016-05-25 17:02:10 -0400

    fixing failing test

--- turtle-client/src/main/java/com/boot/micro/TurtleClientApplication.java
+++ turtle-client/src/main/java/com/boot/micro/TurtleClientApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableCircuitBreaker
 public class TurtleClientApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurtleClientApplication.class, args);
